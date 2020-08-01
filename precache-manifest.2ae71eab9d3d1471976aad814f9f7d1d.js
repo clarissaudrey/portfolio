e1c11d0d2d82b5242d4c2d4c62d756e8425d6b5a
Clarissa Audrey <clrslkmn@students.cs.ubc.ca>
2020-08-01 08:41:57 -0700

    Updates

---
+++ precache-manifest.2ae71eab9d3d1471976aad814f9f7d1d.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9160ba71e5ad65ffbdd02bc14242c420",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "5507ebea62a968012ed1",
+    "url": "/portfolio/static/css/main.0e29b78c.chunk.css"
+  },
+  {
+    "revision": "81e1c8af594c82087e65",
+    "url": "/portfolio/static/js/2.0b150850.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.0b150850.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5507ebea62a968012ed1",
+    "url": "/portfolio/static/js/main.b9413ef7.chunk.js"
+  },
+  {
+    "revision": "2d7c68f33fa3fdf14442",
+    "url": "/portfolio/static/js/runtime-main.6e624f87.js"
+  },
+  {
+    "revision": "de80b0ea4543251af52b6842bbde76c3",
+    "url": "/portfolio/static/media/car.de80b0ea.jpg"
+  },
+  {
+    "revision": "e18749ba32d914d415a18328190153d7",
+    "url": "/portfolio/static/media/class.e18749ba.JPG"
+  },
+  {
+    "revision": "fabe00ce31ab1353822fe36fc2ce8f2c",
+    "url": "/portfolio/static/media/dict.fabe00ce.jpg"
+  },
+  {
+    "revision": "b3506b3e5e3f72f4512fc00d9ce7b8de",
+    "url": "/portfolio/static/media/dns.b3506b3e.png"
+  },
+  {
+    "revision": "0f2792cefe4154d221f433a81272d81f",
+    "url": "/portfolio/static/media/foodbank.0f2792ce.JPG"
+  },
+  {
+    "revision": "740092406f5f982a0e7da328451747aa",
+    "url": "/portfolio/static/media/geo-removebg-preview.74009240.png"
+  },
+  {
+    "revision": "90b75855ebee8505e8f9f24daa1e9340",
+    "url": "/portfolio/static/media/girlsintech.90b75855.JPG"
+  },
+  {
+    "revision": "3d7206f364e37f1f9524421e98fa40d5",
+    "url": "/portfolio/static/media/indo.3d7206f3.JPG"
+  },
+  {
+    "revision": "e18cd2ff6177b018382900c4a0c8d08f",
+    "url": "/portfolio/static/media/mckay.e18cd2ff.jpg"
+  },
+  {
+    "revision": "dd1dbf13408349c84ea859bc1f87095e",
+    "url": "/portfolio/static/media/myPicture.dd1dbf13.png"
+  },
+  {
+    "revision": "445e4373836d20b5a526842e30c80abc",
+    "url": "/portfolio/static/media/netgear.445e4373.png"
+  },
+  {
+    "revision": "a8d796585bdb48673ebf24f50b63596b",
+    "url": "/portfolio/static/media/spaceinvade.a8d79658.png"
+  },
+  {
+    "revision": "d49fde8ca53e8783027522ebdd2c3102",
+    "url": "/portfolio/static/media/transit.d49fde8c.png"
+  },
+  {
+    "revision": "f73079c1b0b4ea5a54788294fd5a1d13",
+    "url": "/portfolio/static/media/ubc.f73079c1.JPG"
+  },
+  {
+    "revision": "9654a17463962116408658a950292af2",
+    "url": "/portfolio/static/media/vantage.9654a174.JPG"
+  }
+]);
